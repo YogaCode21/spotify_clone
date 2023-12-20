759496e0580b0825f763d1db2860db37d792f4f7
YogaCode21 <yogacode2006@gmail.com>
2023-12-20 20:27:46 +0800

    build: add play icon on card hover
    adding play icon on card hover, adding icon and change navbar padding, and add playlist author on card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,7 +14,46 @@ const Main = () => {
             </div>
             <div className="cardContent">
               <h3>Lofi Music</h3>
+              <p>Lofi Girl</p>
             </div>
+
+            <span className="playIcon">
+              <svg
+                data-encore-id="icon"
+                role="img"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="Svg-sc-ytk21e-0 iYxpxA"
+                width="20px"
+              >
+                <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
+              </svg>
+            </span>
+          </div>
+          <div className="card">
+            <div className="cardImage">
+              <img
+                src="https://images.unsplash.com/photo-1679718890686-7f16f411b460?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="Card Image"
+              />
+            </div>
+            <div className="cardContent">
+              <h3>Lofi Music Lorem ipsum dolor sit amet.</h3>
+              <p>Yoga Dharma</p>
+            </div>
+
+            <span className="playIcon">
+              <svg
+                data-encore-id="icon"
+                role="img"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="Svg-sc-ytk21e-0 iYxpxA"
+                width="20px"
+              >
+                <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
+              </svg>
+            </span>
           </div>
         </div>
       </div>
